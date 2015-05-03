d954d224924098a275c2d8b6a21271050747d838
pgrinaway <win98pro>
2015-05-03 18:11:03 -0400

    Creating a base class for the GB models, as well as allowing a user-defined energy function

--- model.py
+++ model.py
@@ -4,12 +4,13 @@ import pymc
 import numpy as np
 import math
 
+
 class GBFFModel(object):
     """
-    A class representing a Bayesian model fitting GB parameters to solvation free energy data
+    A base class representing a Bayesian model fitting GB parameters to solvation free energy data
     """
 
-    def __init__(self, database, initial_parameters, gbmodel):
+    def __init__(self, database, initial_parameters, gbmodel, energy_function):
         """
         Arguments
         ---------
@@ -19,34 +20,17 @@ class GBFFModel(object):
             Dict containing the starting set of parameters for the model
         ngbmodels : int
             The index of the GB model to sample
+        energy_function : function
+            The function to use in computing the energies of molecules
         """
 
-        for (key, value) in initial_parameters.iteritems():
-            (atomtype, parameter_name) = key.split('_')
-            if parameter_name == 'scalingFactor':
-                stochastic = pymc.Uniform(key, value=value, lower=+0.01, upper=+1.0)
-            elif parameter_name == 'radius':
-                stochastic = pymc.Uniform(key, value=value, lower=0.5, upper=3.5)
-            else:
-                raise Exception("Unrecognized parameter name: %s" % parameter_name)
-            setattr(self, key, stochastic)
-
-        self.ngbmodels = ngbmodels
-        gbmodel_dir = pymc.Dirichlet('gbmodel_dir', np.ones([ngbmodels]))
-        self.gbmodel_prior = pymc.CompletedDirichlet('gbmodel_prior', gbmodel_dir)
-        self.gbmodel = pymc.Categorical('gbmodel', p=self.gbmodel_prior)
-
-
-        log_sigma_min = math.log(0.01) # kcal/mol
-        log_sigma_max = math.log(10.0) # kcal/mol
-        log_sigma_guess = math.log(0.2)
-        self.log_sigma = pymc.Uniform('log_sigma', lower=log_sigma_min, upper=log_sigma_max, value=log_sigma_guess)
-        self.sigma = pymc.Lambda('sigma', lambda log_sigma=self.log_sigma : math.exp(log_sigma))
-        self.tau = pymc.Lambda('tau', lambda sigma=self.sigma: sigma**(-2))
+        self.energy_function = energy_function
 
 
 
-    def create_single_gbmodel(self, database, gbmodel, initial_parameters):
+
+
+    def _create_single_gbmodel(self, database, gbmodel, initial_parameters):
         """
         Generates a PyMC model to sample within one GB model (no Reversible Jump)
 
@@ -73,17 +57,11 @@ class GBFFModel(object):
         log_sigma_guess = math.log(0.2)
 
 
-        for (key, value) in initial_parameters.iteritems():
-            (atomtype, parameter_name) = key.split('_')
-            if parameter_name == 'scalingFactor':
-                stochastic = pymc.Uniform(key, value=value, lower=+0.01, upper=+1.0)
-            elif parameter_name == 'radius':
-                stochastic = pymc.Uniform(key, value=value, lower=0.5, upper=3.5)
-            else:
-                raise Exception("Unrecognized parameter name: %s" % parameter_name)
-
-
         gbffmodel['log_sigma'] = pymc.Uniform('log_sigma', lower=log_sigma_min, upper=log_sigma_max, value=log_sigma_guess)
-        gbffmodel['sigma'] = pymc.Lambda('sigma', lambda log_sigma=log_sigma: math.exp(log_sigma))
-        gbffmodel['tau'] = pymc.Lambda('tau', lambda sigma=sigma: sigma**(-2))
+        gbffmodel['sigma'] = pymc.Lambda('sigma', lambda log_sigma=gbffmodel['log_sigma'] : math.exp(log_sigma))
+        gbffmodel['tau'] = pymc.Lambda('tau', lambda sigma=gbffmodel['sigma']: sigma**(-2))
+
+
+        def __generate_parameter_priors(self, )
+
 
