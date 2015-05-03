473896cff25a3530ef6820092a42a3681c845158
pgrinaway <win98pro>
2015-05-03 17:56:51 -0400

    Continue refactor-- decided to use dict format, as well as mixins for energy computation.

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class GBFFModel(object):
     A class representing a Bayesian model fitting GB parameters to solvation free energy data
     """
 
-    def __init__(self, database, initial_parameters, ngbmodels=3):
+    def __init__(self, database, initial_parameters, gbmodel):
         """
         Arguments
         ---------
@@ -17,8 +17,8 @@ class GBFFModel(object):
             Database of FreeSolv solvation free energy data
         initial_parameters : dict
             Dict containing the starting set of parameters for the model
-        ngbmodels : int, optional
-            The number of GB models to sample. Default 3
+        ngbmodels : int
+            The index of the GB model to sample
         """
 
         for (key, value) in initial_parameters.iteritems():
@@ -43,3 +43,47 @@ class GBFFModel(object):
         self.log_sigma = pymc.Uniform('log_sigma', lower=log_sigma_min, upper=log_sigma_max, value=log_sigma_guess)
         self.sigma = pymc.Lambda('sigma', lambda log_sigma=self.log_sigma : math.exp(log_sigma))
         self.tau = pymc.Lambda('tau', lambda sigma=self.sigma: sigma**(-2))
+
+
+
+    def create_single_gbmodel(self, database, gbmodel, initial_parameters):
+        """
+        Generates a PyMC model to sample within one GB model (no Reversible Jump)
+
+        Arguments
+        ---------
+        database : dict
+            Database of FreeSolv solvation free energy data
+        gbmodel : int
+            The index of the GB model to sample
+        initial_parameters : dict
+            Dict containing the starting set of parameters for the model
+
+        Returns
+        -------
+        gbffmodel : dict
+            A dict containing the nodes of a PyMC model to sample
+
+        """
+
+        gbffmodel = dict()
+
+        log_sigma_min = math.log(0.01) # kcal/mol
+        log_sigma_max = math.log(10.0) # kcal/mol
+        log_sigma_guess = math.log(0.2)
+
+
+        for (key, value) in initial_parameters.iteritems():
+            (atomtype, parameter_name) = key.split('_')
+            if parameter_name == 'scalingFactor':
+                stochastic = pymc.Uniform(key, value=value, lower=+0.01, upper=+1.0)
+            elif parameter_name == 'radius':
+                stochastic = pymc.Uniform(key, value=value, lower=0.5, upper=3.5)
+            else:
+                raise Exception("Unrecognized parameter name: %s" % parameter_name)
+
+
+        gbffmodel['log_sigma'] = pymc.Uniform('log_sigma', lower=log_sigma_min, upper=log_sigma_max, value=log_sigma_guess)
+        gbffmodel['sigma'] = pymc.Lambda('sigma', lambda log_sigma=log_sigma: math.exp(log_sigma))
+        gbffmodel['tau'] = pymc.Lambda('tau', lambda sigma=sigma: sigma**(-2))
+
