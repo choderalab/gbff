09690e8f2f747ea61ac9c1992c50591e7f051dca
pgrinaway <win98pro>
2015-07-14 15:04:06 -0400

    fixed unpickle, part2

--- code/prepare_database.py
+++ code/prepare_database.py
@@ -326,7 +326,9 @@ if __name__=="__main__":
 
 
     database_filepath = os.path.join(os.environ['FREESOLV_PATH'], 'database.pickle')
-    database_raw = pickle.load(database_filepath)
+    database_file = open(database_filepath,'r')
+    database_raw = pickle.load(database_file)
+    database_file.close()
     mol2_directory = os.path.join(os.environ['FREESOLV_PATH'], 'tripos_mol2')
     parameters = read_gbsa_parameters(options.parameters_filename)
     database_prepared = prepare_database(database_raw, options.atomtypes_filename, parameters, mol2_directory)
